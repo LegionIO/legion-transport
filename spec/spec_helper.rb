610f9316a902ae8c38d94d69e3b6e026fce58665
Miverson <matthewdiverson@gmail.com>
2018-12-31 21:54:02 -0600

    Adding in logic for code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,13 @@
-require 'simplecov'
-SimpleCov.start
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+begin
+  require 'simplecov'
+  SimpleCov.start
+  if ENV.key?('CODECOV_TOKEN')
+    require 'codecov'
+    SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  end
+rescue LoadError
+  puts 'Failed to load file for coverage reports, continuing without it'
+end
 
 require 'bundler/setup'
 require 'legion/json'
