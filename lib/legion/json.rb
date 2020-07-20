333f338604dc590e1fe62fc89455979d95248626
Esity <matthewdiverson@gmail.com>
2020-07-20 16:06:56 -0500

    removing oj and jrjackson files

--- lib/legion/json.rb
+++ lib/legion/json.rb
@@ -1,48 +1,23 @@
 require 'legion/json/version'
 require 'legion/json/parse_error'
+require 'multi_json'
 
 module Legion
-  # Used to create JSON objects
   module JSON
     class << self
-      # Set up the JSON parser. This method must be called before any
-      # attempt to use the parser. This method is currently called at
-      # the bottom of this file. The appropriate JSON parser will be
-      # loaded for the current platform.
-      #
-      # @return [Object] parser.
-      def setup!
-        if RUBY_ENGINE == 'jruby'
-          require 'jrjackson'
-          require 'legion/json/jrjackson'
-          @parser = Legion::JSON::JrJackson.new
-        else
-          require 'oj'
-          Oj.default_options = { mode: :compat }
-          require 'legion/json/oj'
-          @parser = Legion::JSON::Oj.new
-        end
+      def parser
+        @parser ||= MultiJson
       end
 
-      # Load (and parse) a JSON string.
-      #
-      # @param string [String]
-      # @return [Object]
-      def load(string)
-        @parser.load(string)
+      def load(string, symbolize_keys = true)
+        parser.load(string, symbolize_keys: symbolize_keys)
       rescue StandardError => e
         raise Legion::JSON::ParseError.build(e, string)
       end
 
-      # Dump (generate) a JSON string from a Ruby object.
-      #
-      # @param object [Object]
-      # @param options [Hash]
-      def dump(object, options = {})
-        @parser.dump(object, options)
+      def dump(object, pretty = false)
+        parser.dump(object, pretty: pretty)
       end
     end
   end
 end
-
-Legion::JSON.setup!
--- lib/legion/json/version.rb
+++ lib/legion/json/version.rb
@@ -1,5 +1,5 @@
 module Legion
   module Json
-    VERSION = '1.0.1'.freeze
+    VERSION = '1.1.0'.freeze
   end
 end
