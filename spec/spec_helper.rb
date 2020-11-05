cff11c1907b3803be97b435c3b631ef12b30360f
Esity <matthewdiverson@gmail.com>
2020-11-05 09:41:57 -0600

    removing min coverage for sonar tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ begin
     formatter SimpleCov::Formatter::SimpleFormatter if ENV.key? 'SONAR_TOKEN'
   end
   SimpleCov.use_merging(true)
-  SimpleCov.minimum_coverage 95
+  SimpleCov.minimum_coverage 95 unless ENV.key? 'SONAR_TOKEN'
 rescue LoadError
   puts 'Failed to load file for coverage reports, continuing without it'
 end
